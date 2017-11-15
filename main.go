5b813254a733579a7b7e22850bb556b004438829
James Tingen <jtingen@vl-jtingen-rtp.qualcomm.com>
2017-11-15 10:00:57 -0500

    Remove stdout bottleneck

--- main.go
+++ main.go
@@ -19,11 +19,12 @@ func main() {
 			coprimes++
 		}
 		
-		pi := math.Sqrt(6/(float64(coprimes)/float64(total)))
+		if total%100000==0 {
+			pi := math.Sqrt(6/(float64(coprimes)/float64(total)))
+			err := math.Abs(math.Pi-pi)/math.Pi * 100
 		
-		err := math.Abs(math.Pi-pi)/math.Pi * 100
-		
-		fmt.Printf("%d %19d %19d %40.38f %E%%\n",total,a,b,pi,err)
+			fmt.Printf("\r%d %19d %19d %40.38f %E%%",total,a,b,pi,err)
+		}
 	}
 }
 
