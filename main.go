b91e0cddc02e2d9aa36de56a02d8ac35c3caf79e
James Tingen <jtingen@vl-jtingen-rtp.qualcomm.com>
2017-11-15 12:16:16 -0500

    Switched to math/big.  Can pass in number as argument to specify number of digits in randomly generated numbers.

--- main.go
+++ main.go
@@ -1,38 +1,65 @@
 package main
 
 import (
-    "fmt"
+	"bytes"
+	"fmt"
 	"math"
-    "math/rand"
+	"math/big"
+	"math/rand"
+	"os"
+	"strconv"
 )
 
 func main() {
+	var err error
+	digits := 20
+	if len(os.Args) == 2 {
+		digits, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println("Not valid integer")
+			os.Exit(1)
+		}
+	}
+	fmt.Println(digits)
+
 	var total int64 = 0
 	var coprimes int64 = 0
-	
-	for ;; {
-		a := rand.Int63()
-		b := rand.Int63()
-		
+
+	a := new(big.Int)
+	b := new(big.Int)
+
+	for {
+
+		var abuffer bytes.Buffer
+		var bbuffer bytes.Buffer
+		for i := 0; i < digits; i++ {
+			abuffer.WriteString(strconv.Itoa(rand.Intn(10)))
+			bbuffer.WriteString(strconv.Itoa(rand.Intn(10)))
+		}
+		a.SetString(abuffer.String(), 10)
+		b.SetString(bbuffer.String(), 10)
+
 		total++
-		if GCD(a,b) == 1 {
+		if IsCoprime(a, b) {
 			coprimes++
 		}
-		
-		if total%100000==0 {
-			pi := math.Sqrt(6/(float64(coprimes)/float64(total)))
-			err := math.Abs(math.Pi-pi)/math.Pi * 100
-		
-			fmt.Printf("\r%d %19d %19d %40.38f %E%%",total,a,b,pi,err)
+
+		if total%100 == 0 {
+			pi := math.Sqrt(6 / (float64(coprimes) / float64(total)))
+			err := math.Abs(math.Pi-pi) / math.Pi * 100
+
+			//fmt.Println()
+			//fmt.Println(a)
+			//fmt.Println(b)
+			fmt.Printf("%d %d %40.38f %E%%\r", total, coprimes, pi, err)
 		}
 	}
 }
 
-func GCD(a, b int64) int64 {
-      for b != 0 {
-              t := b
-              b = a % b
-              a = t
-      }
-      return a
+func IsCoprime(a, b *big.Int) bool {
+	one := big.NewInt(1)
+	gcd := new(big.Int)
+	gcd.GCD(nil, nil, a, b)
+
+	return gcd.Cmp(one) == 0
 }
